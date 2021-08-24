59f39595bc07d844d277dbae66ac257f9cfb3088
Sam Sepiel <pojskicsafet@gmail.com>
2021-08-24 14:25:19 +0200

    Connected to API to get most popular movies and showed them on page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Movie from "./components/Movie";
 
 function App() {
+  const API_KEY = "api_key=95cf3273f53ef50fc7371e3b988593c2";
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const POPULAR_URL =
+    BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+
+  const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+
+  const [movies, setMovies] = useState([]);
+
+  async function fetchData() {
+    const moviesP = await axios(POPULAR_URL);
+    console.log(moviesP.data.results);
+    setMovies(moviesP.data.results);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {movies.map((movie) => (
+        <Movie key={movie.id} {...movie} />
+      ))}
+    </main>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+function Movie({ title, poster_path, overview, vote_average }) {
+  const IMG_URL = "https://image.tmdb.org/t/p/w500";
+
+  const getColor = (vote) => {
+    if (vote >= 8.0) {
+      return "green";
+    } else if (vote < 8.0 && vote >= 5.0) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
+  return (
+    <div className="movie">
+      <img src={IMG_URL + poster_path} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span className={getColor(vote_average)}>{vote_average}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
